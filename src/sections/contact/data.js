ba07fdb27d202669d033de09e8366506752b3639
Jimmy Walker <79887392+Jwalker5577@users.noreply.github.com>
2024-02-25 23:11:33 -0500

    Add files via upload

---
+++ src/sections/contact/data.js
@@ -0,0 +1,17 @@
+import {HiOutlineMail} from 'react-icons/hi'
+import {RiMessengerLine} from 'react-icons/ri'
+import {FaWhatsapp} from 'react-icons/fa'
+
+const data = [
+    {id: 1, icon: <HiOutlineMail/>, link: 'mailto:dummyegator@gmail.com'},
+    {id: 2, icon: <RiMessengerLine/>, link: 'https://walkerproductions.netlify.app/'},
+    {id: 3, icon: <FaWhatsapp/>, link: 'https://walkerproductions.netlify.app/'}
+]
+
+
+// alternative whatsApp link
+// https://wa.me/yournumber
+// https://api.whatsapp.com/send/?phone=%2B233557097546
+
+
+export default data
